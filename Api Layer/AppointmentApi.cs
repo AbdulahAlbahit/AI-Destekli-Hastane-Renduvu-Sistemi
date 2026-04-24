1e2c4ed7da92aac0ec1c3539bb1c26f37d92115b
abdulah <abdulahstudy@gmail.com>
2026-04-24 18:06:01 +0300

    ADHRS-19 : Appointment controller olusturuldu

---
+++ Api Layer/AppointmentApi.cs
@@ -0,0 +1,14 @@
+﻿using Business_Layer.Services;
+
+namespace Api_Layer
+{
+    public class AppointmentApi
+    {
+        private readonly IAppointmentService _service;
+
+        public AppointmentApi(IAppointmentService service)
+        {
+            _service=service;
+        }
+    }
+}
