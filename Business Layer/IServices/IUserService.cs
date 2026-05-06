edaf5f2079461b2e18c1a84bd73c3e1f27de1360
abdulah <abdulahstudy@gmail.com>
2026-05-06 15:31:14 +0300

    ADHRS-28: Login islemi backend tarafinda tamamlandi

--- Business Layer/IServices/IUserService.cs
+++ Business Layer/IServices/IUserService.cs
@@ -5,5 +5,6 @@ namespace Business_Layer.Services
     public interface IUserService
     {
         Task<Users> AddUser(Users user);
+        Task<Users> CheckUser(Users user);
     }
 }
--- Data Accese Layer/IRepos/IUserRepo.cs
+++ Data Accese Layer/IRepos/IUserRepo.cs
@@ -5,5 +5,7 @@ namespace Data_Accese_Layer.IRepos
     public interface IUserRepo
     {
         Task<Users> AddUser(Users user);
+        Task<Users> CheckUser(Users user);
+
     }
 }
